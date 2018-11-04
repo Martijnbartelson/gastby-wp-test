1bab1a75b518cea7401e1c4a902c79aab1ba78c2
Martijn Bartelson <martijn.bartelson@gopublic.nl>
2018-11-04 21:31:20 +0100

    firstcommit

---
+++ src/pages/index.js
@@ -0,0 +1,38 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { graphql } from 'gatsby'
+import Layout from '../components/Layout'
+import PostList from '../components/PostList'
+
+export default class IndexPage extends React.Component {
+  render() {
+    const { data } = this.props
+    const { edges: posts } = data.allWordpressPost
+
+    return (
+      <Layout>
+        <PostList posts={posts} title="Latest posts" />
+      </Layout>
+    )
+  }
+}
+
+IndexPage.propTypes = {
+  data: PropTypes.shape({
+    allWordpressPost: PropTypes.shape({
+      edges: PropTypes.array,
+    }),
+  }),
+}
+
+export const pageQuery = graphql`
+  query IndexQuery {
+    allWordpressPost(sort: { fields: date, order: DESC },filter: {categories: {wordpress_id: {eq: 8}}}) {
+      edges {
+        node {
+          ...PostListFields
+        }
+      }
+    }
+  }
+`
